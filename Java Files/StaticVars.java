155fd4743d25436b9ac882c447cc6142727c53a2
Or Eli Pilosof <44499215+orpilosof21@users.noreply.github.com>
2020-06-13 19:21:47 +0300

    init push of java files

---
+++ Java Files/StaticVars.java
@@ -0,0 +1,24 @@
+import javafx.util.Pair;
+
+import java.util.List;
+
+public class StaticVars {
+    public static int startRow;
+    public static int startCol;
+
+    public static int finishRow;
+    public static int finishCol;
+
+    public static int curRow;
+    public static int curCol;
+
+    public static String grid;
+    public static String algo;
+
+    public static int backtrackMinCount;
+    public static List<Pair<Integer, Integer>> backtrackMinSol;
+
+    public static boolean hasReachTarget;
+
+    //public static GameObject startTile;
+}
